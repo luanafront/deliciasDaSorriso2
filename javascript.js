8b383220aa25af69412626691a756388f460daba
luanafront <luana.lopes.rodrigues1@gmail.com>
2022-03-31 21:00:28 -0300

    alteracao no valor

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// let quantidadeDinheiro = 5
+// let valorDoPao = 1
+// let quantidadeComprada = 2
+// let valorTotalDaCompra = quantidadeComprada * valorDoPao
+
+// let troco = quantidadeDinheiro - valorTotalDaCompra
+
+function comprarPaes(quantidadeDinheiro, valorDoPao, quantidadeDePaes){
+    let valorTotalDaCompra = quantidadeDePaes * valorDoPao 
+    let troco = quantidadeDinheiro - valorTotalDaCompra
+    return troco 
+}
